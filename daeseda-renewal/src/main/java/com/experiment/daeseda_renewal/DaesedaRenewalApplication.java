60ba8b0f14157e041a1ea7d9794c4aeb132bc53f
zkvn1103 <zkvn1103@naver.com>
2025-01-02 20:04:01 +0900

    init project

---
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/DaesedaRenewalApplication.java
@@ -0,0 +1,13 @@
+package com.experiment.daeseda_renewal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaesedaRenewalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaesedaRenewalApplication.class, args);
+	}
+
+}
---
+++ daeseda-renewal/src/test/java/com/experiment/daeseda_renewal/DaesedaRenewalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiment.daeseda_renewal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaesedaRenewalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
