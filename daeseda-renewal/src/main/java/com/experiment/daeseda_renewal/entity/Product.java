cc0f73138633697327de0d7c763c99d816372f7f
zkvn1103 <zkvn1103@naver.com>
2025-01-13 20:38:11 +0900

    add OrderItem, Product Entity

---
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/entity/Product.java
@@ -0,0 +1,29 @@
+package com.experiment.daeseda_renewal.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private BigDecimal price;
+}
