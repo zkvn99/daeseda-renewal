1bb2d5a401ff570f6420dfa26c7b3ea6c03700de
minwook <zkvn1103@naver.com>
2025-01-18 15:51:57 +0900

    edit OrderController

---
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/controller/OrderController.java
@@ -0,0 +1,9 @@
+package com.experiment.daeseda_renewal.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class OrderController {
+}
