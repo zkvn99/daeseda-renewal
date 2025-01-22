e7c559c379cc56c1bfea4b6874a012362b449eb2
zkvn1103 <zkvn1103@naver.com>
2025-01-22 20:03:10 +0900

    add UserTest

---
+++ daeseda-renewal/src/test/java/com/experiment/daeseda_renewal/service/UserServiceTest.java
@@ -0,0 +1,45 @@
+package com.experiment.daeseda_renewal.service;
+
+import com.experiment.daeseda_renewal.dto.UserDTO;
+import com.experiment.daeseda_renewal.entity.User;
+import com.experiment.daeseda_renewal.repository.UserRepository;
+import com.experiment.daeseda_renewal.service.user.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @MockitoBean
+    private UserRepository userRepository;
+
+    private UserService userService;
+
+    @Test
+    void testSignup() {
+        // given
+        UserDTO userDTO = UserDTO.builder()
+                .email("email@email.com")
+                .name("name")
+                .password("password")
+                .build();
+
+        User user = User.builder()
+                .email(userDTO.getEmail())
+                .name(userDTO.getName())
+                .password(userDTO.getPassword())
+                .build();
+
+        User savedUser = User.builder()
+                .email("email@email.com")
+                .name("name")
+                .password("password")
+                .build();
+
+        // when
+
+        // then
+    }
+
+}
