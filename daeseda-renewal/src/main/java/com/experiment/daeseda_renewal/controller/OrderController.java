e93be9b1a943e5bcb3378ee84ab6b2edb638e343
zkvn1103 <zkvn1103@naver.com>
2025-01-30 15:59:31 +0900

    edit Order Confirmation

--- daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/controller/OrderController.java
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/controller/OrderController.java
@@ -1,9 +1,12 @@
 package com.experiment.daeseda_renewal.controller;
 
+import com.experiment.daeseda_renewal.dto.OrderDto;
 import com.experiment.daeseda_renewal.service.order.OrderService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -16,14 +19,15 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/place")
-    public String placeOrder(HttpSession session, RedirectAttributes redirectAttributes) {
+    public String placeOrder(HttpSession session, RedirectAttributes redirectAttributes, Model model) {
         try {
             Long userId = (Long) session.getAttribute("userId");
-            orderService.placeOrder(userId);
-            return "redirect:/order/confirmation";
+            OrderDto orderDto = orderService.placeOrder(userId);
+            model.addAttribute("order", orderDto);
+            return "/order/confirmation";
         } catch (IllegalStateException e) {
             redirectAttributes.addFlashAttribute("error", e.getMessage());
-            return "redirect:/cart/list";  // 장바구니가 비어있으면 다시 장바구니 페이지로 이동
+            return "/cart/list";  // 장바구니가 비어있으면 다시 장바구니 페이지로 이동
         }
     }
 }
