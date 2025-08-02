87e51c99374ea76342e46e40de9a617f404a7fdd
zkvn1103@naver.com <zkvn1103@naver.com>
2025-08-02 14:44:16 +0900

    [구조] 주석 수정

--- daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/constant/ErrorCode.java
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/constant/ErrorCode.java
@@ -6,20 +6,32 @@ import org.springframework.boot.logging.LogLevel;
 import org.springframework.http.HttpStatus;
 
 /**
- * [형식] - code: 도메인 약어 - 행위 - 일련번호 (예: ADDR-CREATE-001) - message: 사용자에게 보여줄 메시지 (자연어) - httpStatus:
- * 상황에 맞는 HTTP 상태 코드 400 Bad Request             요청 자체가 잘못됨, 유효성 실패, 파라미터 누락, 요청값에 문제 있음 401
- * Unauthorized            인증 실패, 로그인 실패, 로그인 안 했거나 세션 만료 403 Forbidden	            권한 없음, 관리자 전용
- * 접근, 권한 부족, 로그인했지만 권한 없음 404 Not Found	            찾을 수 없음, 유저 ID 없음, 주소 없음, 정상 요청이지만 대상 없음 409
- * Conflict	            중복/충돌	중복 이메일, 중복 게시글 422 Unprocessable Entity	데이터는 OK, 의미가 없음	날짜 범위가 잘못됨	논리적
- * 문제 있음 500 Internal Server Error   서버 오류, DB 장애 - logLevel: 로그 기록 시 사용할 로그 수준 단순 유효성 실패는 INFO 또는
- * WARN, 시스템 장애는 ERROR로 logLevel 지정
- * <p>
- * [규칙] 1. 도메인(Domain): 기능별 책임 단위의 약어 (예: USER, ADDR, PROD, ORDER 등) 2. 행위(Action): CREATE, DELETE,
- * UPDATE, LOGIN, VALID 등 핵심 동작을 대문자로 표현 3. 일련번호(Number): 동일 도메인+행위 조합 내에서 고유하게 증가 (3자리 숫자 권장) 4.
- * 중복되거나 유사한 메시지는 피하고, 사용자와 개발자 모두에게 명확하게 전달될 수 있도록 작성
- * <p>
- * [예시] - USER-LOGIN-001: 로그인 실패 (비밀번호 불일치 등) - ADDR-DELETE-002: 주소 삭제 권한 없음 - PROD-CREATE-003: 상품
- * 등록 중 서버 오류 발생
+ *
+ * [형식]
+ *   - code: 도메인 약어 - 행위 - 일련번호 (예: ADDR-CREATE-001)
+ *   - message: 사용자에게 보여줄 메시지 (자연어)
+ *   - httpStatus: 상황에 맞는 HTTP 상태 코드
+ *          400 Bad Request             요청 자체가 잘못됨, 유효성 실패, 파라미터 누락, 요청값에 문제 있음
+ *          401 Unauthorized            인증 실패, 로그인 실패, 로그인 안 했거나 세션 만료
+ *          403 Forbidden	            권한 없음, 관리자 전용 접근, 권한 부족, 로그인했지만 권한 없음
+ *          404 Not Found	            찾을 수 없음, 유저 ID 없음, 주소 없음, 정상 요청이지만 대상 없음
+ *          409 Conflict	            중복/충돌	중복 이메일, 중복 게시글
+ *          422 Unprocessable Entity	데이터는 OK, 의미가 없음	날짜 범위가 잘못됨	논리적 문제 있음
+ *          500 Internal Server Error   서버 오류, DB 장애
+ *   - logLevel: 로그 기록 시 사용할 로그 수준
+ *          단순 유효성 실패는 INFO 또는 WARN, 시스템 장애는 ERROR로 logLevel 지정
+ *
+ * [규칙]
+ *   1. 도메인(Domain): 기능별 책임 단위의 약어 (예: USER, ADDR, PROD, ORDER 등)
+ *   2. 행위(Action): CREATE, DELETE, UPDATE, LOGIN, VALID 등 핵심 동작을 대문자로 표현
+ *   3. 일련번호(Number): 동일 도메인+행위 조합 내에서 고유하게 증가 (3자리 숫자 권장)
+ *   4. 중복되거나 유사한 메시지는 피하고, 사용자와 개발자 모두에게 명확하게 전달될 수 있도록 작성
+ *
+ * [예시]
+ *   - USER-LOGIN-001: 로그인 실패 (비밀번호 불일치 등)
+ *   - ADDR-DELETE-002: 주소 삭제 권한 없음
+ *   - PROD-CREATE-003: 상품 등록 중 서버 오류 발생
+ *
  */
 
 
