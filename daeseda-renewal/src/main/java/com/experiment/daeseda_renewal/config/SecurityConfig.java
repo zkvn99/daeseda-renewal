4709081d6f5e311863088fd0fc2c4e8df2fd127a
zkvn1103@naver.com <zkvn1103@naver.com>
2025-08-03 21:38:57 +0900

    [제거] 필터 체인 주석

--- daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/config/SecurityConfig.java
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/config/SecurityConfig.java
@@ -2,31 +2,28 @@ package com.experiment.daeseda_renewal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth // ✅ 새로운 방식
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults()) // 기본 인증 방식
-                .csrf(csrf -> csrf.disable()); // ✅ Lambda DSL로 CSRF 비활성화
-        return http.build();  // HttpSecurity 설정을 반환
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth // ✅ 새로운 방식
+//                        .requestMatchers("/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .httpBasic(Customizer.withDefaults()) // 기본 인증 방식
+//                .csrf(csrf -> csrf.disable()); // ✅ Lambda DSL로 CSRF 비활성화
+//        return http.build();  // HttpSecurity 설정을 반환
+//    }
 }
