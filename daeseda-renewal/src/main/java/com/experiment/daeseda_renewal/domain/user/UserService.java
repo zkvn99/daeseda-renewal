dbf98a318cee14007cafbaeeba6ce475bf35eec4
zkvn1103@naver.com <zkvn1103@naver.com>
2025-08-06 18:44:34 +0900

    [수정] 메서드 변경

--- daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/domain/user/UserService.java
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/domain/user/UserService.java
@@ -1,8 +1,12 @@
 package com.experiment.daeseda_renewal.domain.user;
 
 import com.experiment.daeseda_renewal.domain.user.dto.CreateUserRequest;
+import com.experiment.daeseda_renewal.domain.user.dto.DeleteUserRequest;
+import com.experiment.daeseda_renewal.domain.user.dto.FindUserEmailRequest;
 import com.experiment.daeseda_renewal.domain.user.dto.LoginRequest;
 import com.experiment.daeseda_renewal.domain.user.dto.LoginResponse;
+import com.experiment.daeseda_renewal.domain.user.dto.ResetUserPasswordRequest;
+import com.experiment.daeseda_renewal.domain.user.dto.VerifyUserPasswordResetRequest;
 
 public interface UserService {
 
@@ -12,9 +16,13 @@ public interface UserService {
 
   LoginResponse login(LoginRequest request);
 
-  String findEmailByName(String name);
+  String findUserEmail(FindUserEmailRequest request);
 
   boolean isEmailDuplicate(String email);
 
-  void delete(UserDto userDto);
+  void delete(DeleteUserRequest request);
+
+  boolean verifyUserBeforePasswordReset(VerifyUserPasswordResetRequest request);
+  
+  void resetUserPassword(ResetUserPasswordRequest request);
 }
--- daeseda-renewal/src/test/java/com/experiment/daeseda_renewal/service/UserServiceTest.java
+++ daeseda-renewal/src/test/java/com/experiment/daeseda_renewal/service/UserServiceTest.java
@@ -1,9 +1,9 @@
 package com.experiment.daeseda_renewal.service;
 
 import com.experiment.daeseda_renewal.domain.user.User;
-import com.experiment.daeseda_renewal.domain.user.UserDto;
 import com.experiment.daeseda_renewal.domain.user.UserRepository;
 import com.experiment.daeseda_renewal.domain.user.UserService;
+import com.experiment.daeseda_renewal.domain.user.dto.CreateUserRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -11,35 +11,41 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 @SpringBootTest
 public class UserServiceTest {
 
-    @MockitoBean
-    private UserRepository userRepository;
+  @MockitoBean
+  private UserRepository userRepository;
 
-    private UserService userService;
+  private UserService userService;
 
-    @Test
-    void testSignup() {
-        // given
-        UserDto userDTO = UserDto.builder()
-                .email("email@email.com")
-                .name("name")
-                .password("password")
-                .build();
+  @Test
+  void testSignup() {
+    // given
+    CreateUserRequest request = CreateUserRequest.builder()
+                                                 .userEmail("email@email.com")
+                                                 .userName("name")
+                                                 .userPassword("password")
+                                                 .userNickname("nickname")
+                                                 .userPhone("0101412452")
+                                                 .build();
 
-        User user = User.builder()
-                .email(userDTO.getEmail())
-                .name(userDTO.getName())
-                .password(userDTO.getPassword())
-                .build();
+    User user = User.builder()
+                    .userEmail(request.getUserEmail())
+                    .userName(request.getUserName())
+                    .userPassword(request.getUserPassword())
+                    .userNickname(request.getUserNickname())
+                    .userPhone(request.getUserPhone())
+                    .build();
 
-        User savedUser = User.builder()
-                .email("email@email.com")
-                .name("name")
-                .password("password")
-                .build();
+    User savedUser = User.builder()
+                         .userEmail("email@email.com")
+                         .userName("name")
+                         .userPassword("password")
+                         .userNickname("nickname")
+                         .userPhone("0101412452")
+                         .build();
 
-        // when
+    // when
 
-        // then
-    }
+    // then
+  }
 
 }
