b403cc9dc8002b7647ca00e32d30157f8179bb61
zkvn1103@naver.com <zkvn1103@naver.com>
2025-08-09 16:48:12 +0900

    [수정] 주소 복수형 변경

--- daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/domain/address/AddressController.java
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/domain/address/AddressController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/address")
+@RequestMapping("/addresses")
 public class AddressController {
 
   private final AddressService addressService;
--- daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/domain/address/api/AddressApiController.java
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/domain/address/api/AddressApiController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/address")
+@RequestMapping("/api/addresses")
 public class AddressApiController {
 
   private final AddressService addressService;
