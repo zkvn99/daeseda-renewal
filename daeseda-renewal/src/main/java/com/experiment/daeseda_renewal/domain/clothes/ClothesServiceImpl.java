a7255533470757607dc7ae41cb4006732eb49cd0
backminjoo <qoralswn1205@daum.net>
2025-08-10 15:04:50 +0900

    [기능]의류 작업

--- daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/domain/clothes/ClothesServiceImpl.java
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/domain/clothes/ClothesServiceImpl.java
@@ -1,33 +1,43 @@
 package com.experiment.daeseda_renewal.domain.clothes;
 
+import com.experiment.daeseda_renewal.domain.clothes.dto.CreateClothesRequest;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ClothesServiceImpl implements ClothesService {
 
-    private final ClothesRepository clothesRepository;
+  private final ClothesRepository clothesRepository;
 
-    @Override
-    public List<Clothes> getAllClothes() {
-        return clothesRepository.findAll();
+  // 의류 리스트 조회
+  @Override
+  public List<Clothes> getAllClothes() {
+    return clothesRepository.findAll();
+  }
+
+  // 의류 생성
+  @Override
+  public Clothes createClothes(CreateClothesRequest createClothesRequest) {
+    Clothes clothes = convertToEntity(createClothesRequest);
+    Clothes savedClothes = clothesRepository.save(clothes);
+    return savedClothes;
+  }
+
+  @Override
+  public CreateClothesRequest getClothesById(Long clothesById) {
+    return null;
+  }
+
+  // 의류 삭제
+  @Override
+  public int deleteClothes(Long clothesId) {
+    try {
+      clothesRepository.deleteById(clothesId);
+      return 1; // 삭제 성공 시 1 반환
+    } catch (Exception e) {
+      return 0; // 삭제 실패 시 0 반환
     }
-
-    @Override
-    public Clothes createClothes(CreateClothesDTO createClothesDTO) {
-        Clothes clothes = convertToEntity(createClothesDTO);
-        Clothes savedClothes = clothesRepository.save(clothes);
-        return savedClothes;
-    }
-
-    @Override
-    public CreateClothesDTO getClothesById(Long clothesById) {
-        return null;
-    }
-
+  }
 }
--- daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/domain/clothes/api/ClothesApiController.java
+++ daeseda-renewal/src/main/java/com/experiment/daeseda_renewal/domain/clothes/api/ClothesApiController.java
@@ -1,24 +1,38 @@
 package com.experiment.daeseda_renewal.domain.clothes.api;
 
 import com.experiment.daeseda_renewal.domain.clothes.ClothesService;
-import com.experiment.daeseda_renewal.domain.clothes.CreateClothesDTO;
+import com.experiment.daeseda_renewal.domain.clothes.dto.CreateClothesRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/clothes")
+@RequestMapping("/api/clothes")
 @RequiredArgsConstructor
 public class ClothesApiController {
-    private final ClothesService clothesService;
 
-    @PostMapping("/register")
-    public ResponseEntity<String> registerClothes(@RequestBody CreateClothesDTO clothesDTO) {
-        clothesService.createClothes(clothesDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+  private final ClothesService clothesService;
+
+  // 의류 생성
+  @PostMapping("/register")
+  public ResponseEntity<String> registerClothes(@RequestBody CreateClothesRequest clothesDTO) {
+    clothesService.createClothes(clothesDTO);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  // 의류 삭제
+  @DeleteMapping("/{clothesId}")
+  public ResponseEntity<String> deleteClothes(@PathVariable Long clothesId) {
+    if (clothesService.deleteClothes(clothesId) > 0) {
+      return ResponseEntity.status(HttpStatus.NO_CONTENT).body("삭제가 성공적으로 실행되었습니다");
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("삭제에 실패했습니다.");
     }
+  }
 }
